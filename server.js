eb7ce255f63dee9d769e31b2ff8938e530216c05
Minhajul Islam <mmminhaj221@gmail.com>
2026-08-16 14:13:23 +0600

    integrated postgray sql and prisma orm for database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require("./app");
+// const app = require("./app");
+import app from "./app.js"
 const port = 3000 ;
+import prisma from "./src/config/prisma.js";
 
+async function testDatabase(params) {
+    const users = await prisma.user.findMany();
+    console.log(users)
+}
+testDatabase()
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`);
 });
